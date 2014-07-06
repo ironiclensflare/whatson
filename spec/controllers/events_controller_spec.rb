8960596b637be58c4bb569ae57c96f0d623c7a1b
Stu <stu@ironiclensflare.com>
2014-07-06 13:46:30 +0100

    Started adding tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, :type => :controller do
-
+  it "has all environment variables populated" do
+    username = ENV['GCAL_EMAIL']
+    password = ENV['GCAL_PASS']
+    appname  = ENV['GCAL_NAME']
+    
+    expect(username).to be_truthy
+    expect(password).to be_truthy
+    expect(appname).to be_truthy
+  end
 end
